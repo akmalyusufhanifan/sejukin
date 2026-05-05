c3e2d6f6ae2214d05f697319267e0a40a464033d
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2026-05-05 14:57:33 +0700

    add navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,27 @@
 
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    setShowNav(true);
+  }, []);
 
   return (
     <>
-      <nav className="sticky top-0 flex justify-between items-center bg-white h-16 px-8 shadow lg:px-40">
+      <nav
+        className={`fixed top-0 left-0 w-full flex justify-between items-center bg-white h-16 px-8 lg:px-40 z-50
+  transform transition-all duration-1500 ease-[cubic-bezier(0.22,1,0.36,1)]
+  ${
+    showNav
+      ? "translate-y-0 opacity-100 shadow"
+      : "-translate-y-6 opacity-0 shadow-none"
+  }`}
+      >
         {/* logo */}
         <div className="text-3xl text-black">Sejuk.In</div>
 
@@ -44,14 +57,14 @@ export default function NavBar() {
       {/* overlay */}
       <div
         onClick={() => setIsOpen(false)}
-        className={`fixed inset-0 bg-black/5 backdrop-blur-xs z-40 md:hidden transition-opacity ${
+        className={`fixed inset-0 bg-black/5 backdrop-blur-xs z-60 md:hidden transition-opacity ${
           isOpen ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       />
 
       {/* navigation slide */}
       <div
-        className={`fixed top-0 right-0 h-full w-64 md:hidden bg-white shadow-xl z-50 transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full w-64 md:hidden bg-white shadow-xl z-65 transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
