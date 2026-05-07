45c2258f08c508cd6ae35212d3dabec45943578b
akmalyusufhanifan <akmalyusufhanifan@gmail.com>
2026-05-07 14:00:46 +0700

    add new section: about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/sections/banner";
+import AboutUs from "@/sections/about-us";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center pt-16">
+    <div className="min-h-fit bg-white flex flex-col items-center pt-16 space-y-8 md:space-y-12">
       <Banner />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/sections/about-us.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function AboutUs() {
+  return (
+    <section className="min-h-fit w-full flex flex-col items-center px-8 space-y-8 lg:flex-row lg:justify-between lg:px-40">
+      <div>
+        <Image
+          src="/about-us-image.webp"
+          alt="Sejukin Image Description"
+          width={500}
+          height={300}
+        />
+      </div>
+
+      <div className="flex flex-col items-start space-y-4 md:max-w-2xl">
+        <h2 className="text-3xl text-sky-700 font-semibold">About Us</h2>
+        <p className="text-lg leading-8 text-gray-700">
+          Sejukin is a professional air conditioning service and maintenance
+          company dedicated to keeping homes and businesses cool, clean, and
+          comfortable. We provide reliable AC servicing with fast response times
+          and high-quality workmanship.
+        </p>
+
+        <p className="text-lg leading-8 text-gray-700">
+          Our strength lies in our experienced technicians, transparent service,
+          and commitment to customer satisfaction. With modern solutions and
+          dependable support, Sejukin helps customers maintain efficient and
+          long-lasting air conditioning systems.
+        </p>
+      </div>
+    </section>
+  );
+}
